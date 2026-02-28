dc14bd680372459fbcb7c047f5fc1c20995f8c1a
xlwang@ <xinlongwang@m.ldu.edu.cn>
2026-02-28 20:26:45 +0800

    Update __init__.py

--- zsd_detr/modeling/__init__.py
+++ zsd_detr/modeling/__init__.py
@@ -1,5 +1,6 @@
 from .zsd_detr import ZSDDETR
 from .zsd_detr_criterion import ZSDDETRCriterion
 from .zsd_detr_transformer import ZSDDETRTransformer, ZSDDETRTransformerDecoder, ZSDDETRTransformerEncoder
-
-__all__ = ["ZSDDETR", "ZSDDETRCriterion", "ZSDDETRTransformer", "ZSDDETRTransformerDecoder", "ZSDDETRTransformerEncoder"]
+from .visual_prompts import *
+from .FFTVL import *
+__all__ = ["ZSDDETR", "ZSDDETRCriterion", "ZSDDETRTransformer", "ZSDDETRTransformerDecoder", "ZSDDETRTransformerEncoder", "VisualPromptsEncoder", "FFTBiVLFuse"]
